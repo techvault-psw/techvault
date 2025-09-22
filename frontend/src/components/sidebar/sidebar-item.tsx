1c21c52ea5a5ad00b4e940b8d5475b53cdc3f0b3
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2025-09-21 21:54:51 -0300

    fix: corrigindo problema da Sidebar não fechar ao clicar em um link

--- frontend/src/components/sidebar/sidebar-item.tsx
+++ frontend/src/components/sidebar/sidebar-item.tsx
@@ -5,11 +5,13 @@ import { Link, type LinkProps } from "react-router";
 interface SidebarItemProps extends LinkProps {
   display: string
   icon: ComponentType<SVGProps<SVGSVGElement>>;
+  closeSidebar: () => void;
 }
 
-export const SidebarItem = ({ display, icon: Icon, className, ...props }: SidebarItemProps) => {
+export const SidebarItem = ({ display, icon: Icon, closeSidebar, className, ...props }: SidebarItemProps) => {
   return (
     <Link
+      onClick={closeSidebar}
       className={cn("py-1 pl-1.5 -ml-1.5 flex items-center gap-2 text-xl text-white leading-[120%] cursor-pointer hover:bg-white/10 transition-colors transition-300 rounded-lg", className)}
       {...props}
     >
