e67f05007d87c8463dab44689e9ed410bb8fd0a9
g20b-cd <gabrielbarretto20@gmail.com>
2025-09-27 17:15:05 -0300

    Atualização root-reducer

--- frontend/src/redux/root-reducer.ts
+++ frontend/src/redux/root-reducer.ts
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
+import { clienteReducer } from "./clientes/slice";
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({clienteReducer})
 
 export default rootReducer
+export type RootState = ReturnType<typeof rootReducer>;
