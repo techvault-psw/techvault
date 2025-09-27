1fcc4f18a5cc3ae67f56e26d10ed49abdf9665e2
devguialmeida <guilherme.almeida.data@gmail.com>
2025-09-27 16:05:32 -0300

    fix: adicionar reducers de Reserva

--- frontend/src/redux/root-reducer.ts
+++ frontend/src/redux/root-reducer.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import { reservasReducer } from "./reservas/slice";
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({reservasReducer})
 
 export default rootReducer
