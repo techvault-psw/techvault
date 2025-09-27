8890671df76a734f3fbb4fcb9f2a3cb5a129be98
g20b-cd <gabrielbarretto20@gmail.com>
2025-09-27 19:13:55 -0300

    Atualizando página de login

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -19,6 +19,9 @@ import InputPassword from '@/components/ui/input-password';
 import { Button } from '../components/ui/button';
 import { Separator } from "@/components/ui/separator";
 import useCargo from "@/hooks/useCargo";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "@/redux/root-reducer";
+import { loginCliente } from "@/redux/clientes/slice";
 
 const formSchema = z.object({
   email: z.string().min(1, "O e-mail é obrigatório").email("Digite um e-mail válido"),
@@ -37,12 +40,28 @@ export default function LoginPage() {
     },
   })
 
+  const dispatch = useDispatch();
+  const {clientes,clienteAtual} = useSelector((state: RootState) => state.clienteReducer);
   function onSubmit(values: z.infer<typeof formSchema>) {
-    if(values.password != "gerente" && values.password != "suporte") {
-      navigate("/")
-    } else {
-      setCargo(values.password)
-      navigate("/dashboard")
+    
+    const cliente = clientes.find(cliente => cliente.email === values.email && cliente.password === values.password);
+    if(cliente){
+      dispatch(loginCliente(values)); 
+      if(cliente.role === "Gerente" || cliente.role === "Suporte"){
+        navigate("/dashboard");
+        setCargo(cliente.role.toLocaleLowerCase());
+      }
+      else{
+        navigate("/");
+      }
+    }
+    else{
+      form2.setError("email",{
+        type: "custom", message: "E-mail ou Senha inválidos"
+      });
+      form2.setError("password",{
+        type: "custom", message: "E-mail ou Senha inválidos"
+      });
     }
   }
 
