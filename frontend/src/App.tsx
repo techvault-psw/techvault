e38be5914e073d6fff44ab0d1133987da8d889da
g20b-cd <gabrielbarretto20@gmail.com>
2025-09-28 17:53:15 -0300

    Editando e integrando backend mockado com as outras funcionalidades do cliente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,6 +19,10 @@ import ReservaConfirmadaPage from "./pages/reserva-confirmada";
 import PerfilPage from "./pages/perfil";
 import Pacotes from "./pages/pacotes";
 import FeedbacksPage from "./pages/feedbacks";
+import store from "./redux/store";
+import { fetchClientes } from "./redux/clientes/fetch";
+
+store.dispatch(fetchClientes());
 
 export default function App() {
   return (
