995f0df19b64720365f1f6177e8f3a2ae44bf600
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2025-09-29 13:31:22 -0300

    fix(deploy): corrigindo deploy na Vercel

--- frontend/api/json-server.js
+++ frontend/api/json-server.js
@@ -1,14 +1,10 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+import jsonServer from "json-server";
 
-const router = jsonServer.router('backend-mock.json')
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router("backend-mock.json");
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
+server.use("/api", router);
 
-server.use('/api', router)
-
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  server(req, res)
-}
+export default server;
