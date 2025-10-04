56dc0badb74f776ef165d7c68174d3488dc52104
g20b-cd <gabrielbarretto20@gmail.com>
2025-10-04 20:57:11 -0300

    Atualização do fetch dos clientes para integração com backend mockado

--- frontend/src/redux/clientes/fetch.ts
+++ frontend/src/redux/clientes/fetch.ts
@@ -1,10 +1,46 @@
 import { API_URL } from "@/lib/api-url"
 import { createAsyncThunk } from "@reduxjs/toolkit"
+import type { ClienteServer, NewClienteServer, Cliente, NewCliente } from "./slice"
+import { httpDelete, httpGet, httpPost, httpPut } from "@/lib/fetch-utils"
 
-export const fetchClientes = createAsyncThunk('clientes/fetchClientes',
+export const fetchClientes = createAsyncThunk<Cliente[]>(`clientes/fetchClientes`,
   async () => {
-    const response = await fetch(`${API_URL}/clientes`)
-    const data = await response.json()
-    return { clientes: data }
+    return await httpGet('/clientes') 
   }
 )
+
+export const addClienteServer = createAsyncThunk<Cliente, NewCliente>('clientes/addClienteServer ',
+  async (newCliente) => {
+    const cliente: NewClienteServer = {
+      ...newCliente
+    }
+
+    return await httpPost('/clientes', cliente)
+  }
+)
+
+export const deleteClienteServer = createAsyncThunk<number , Cliente>('clientes/deleteClienteServer ',
+  async (Cliente) => {
+    await httpDelete(`/clientes/${Cliente.id}`)
+    return Cliente.id;
+  }
+)
+
+export const updateClienteServer = createAsyncThunk<Cliente, Cliente>('clientes/updateClienteServer ',
+  async (Cliente) => {
+    const updatedCliente: ClienteServer = {
+      ...Cliente
+    }
+
+    return await httpPut(`/clientes/${Cliente.id}`, updatedCliente)
+  }
+)
+
+
+
+  
+
+
+
+
+
