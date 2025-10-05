38f38154068bef3a8ef61ef15da63cda647ee7d3
Matheus Cunha Schwab <matheus.cunha.schwab@gmail.com>
2025-10-04 21:48:38 -0300

    Atualizacao arquivo App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,14 +20,12 @@ import PerfilPage from "./pages/perfil";
 import Pacotes from "./pages/pacotes";
 import FeedbacksPage from "./pages/feedbacks";
 import { fetchReservas } from "./redux/reservas/fetch";
-import { fetchPacotes } from "./redux/pacotes/fetch";
 import { fetchClientes } from "./redux/clientes/fetch";
 import { fetchEnderecos } from './redux/endereco/fetch.ts'
 import store from './redux/store.ts'
 
 store.dispatch(fetchClientes());
 store.dispatch(fetchEnderecos())
-store.dispatch(fetchPacotes());
 store.dispatch(fetchReservas());
 
 export default function App() {
