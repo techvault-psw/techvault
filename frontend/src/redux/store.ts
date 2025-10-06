dbb17271629779d5883c0083c357fef0d0ad1127
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2025-10-06 20:36:53 -0300

    feat: persistir o estado clienteAtual usando redux-persist

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,9 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import rootReducer from "./root-reducer.ts";
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch
 
