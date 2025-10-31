cf36ffe904dbfb61906f10ccf9eea85d81d88b20
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2025-10-30 22:15:13 -0300

    fix: configurando CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import router from './routes'
+import cors from 'cors'
 import swaggerUi from 'swagger-ui-express';
 import { generateOpenAPISpec, RequestValidationError, ResponseValidationError, setDefaultResponses, setGlobalErrorHandler } from 'express-zod-openapi-typed';
 import { z } from 'zod'
@@ -7,6 +8,8 @@ import { z } from 'zod'
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use(router)
 
