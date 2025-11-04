82152226cb1922735d0006ecd987cc176a794e26
devguialmeida <guilherme.almeida.data@gmail.com>
2025-11-03 22:50:49 -0300

    feature: adicionada funcionalidade de atualizar dados de uma reserva

---
+++ backend/src/routes/reservas/update-reserva.ts
@@ -0,0 +1,54 @@
+import { CreateTypedRouter } from "express-zod-openapi-typed";
+import z from "zod";
+import { reservas } from "../../consts/db-mock";
+import { reservaZodSchema } from "../../consts/zod-schemas";
+
+const router = CreateTypedRouter()
+
+router.put('/reservas/:id', {
+  schema: {
+    summary: 'Update Reserva',
+    tags: ['Reservas'],
+    params: z.object({
+      id: z.string().uuid(),
+    }),
+    body: reservaZodSchema.omit({
+      id: true,
+      clienteId: true,
+      pacoteId: true,
+      enderecoId: true,
+      valor: true,
+    }),
+    response: {
+      200: reservaZodSchema,
+      400: z.object({
+        success: z.boolean(),
+        message: z.string(),
+      }),
+    },
+  },
+}, async (req, res) => {
+  const { id } = req.params
+  const {dataEntrega, dataColeta, dataInicio, dataTermino } = req.body // agora pode registrar coleta e entrega
+
+  const reservaIndex = reservas.findIndex((reserva) => reserva.id === id)
+
+  if (reservaIndex < 0) {
+    return res.status(400).send({
+      success: false,
+      message: 'Reserva não encontrada'
+    })
+  }
+  
+  reservas[reservaIndex].dataEntrega = dataEntrega
+  reservas[reservaIndex].dataColeta = dataColeta
+  reservas[reservaIndex].dataInicio = dataInicio
+  reservas[reservaIndex].dataTermino = dataTermino
+
+  return res.status(200).send({
+    ...reservas[reservaIndex],
+  })
+
+})
+
+export const updateReserva = router
