a6f5408fb9c133941c4fd99f89ff162fdfbf0d3b
g20b-cd <gabrielbarretto20@gmail.com>
2025-11-10 18:48:01 -0300

    Atualização de get-cliente para models

---
+++ backend/src/routes/cliente/get-clientes.ts
@@ -0,0 +1,26 @@
+import { CreateTypedRouter } from "express-zod-openapi-typed";
+import z from "zod";
+import type { Cliente } from "../../consts/types";
+import { clienteZodSchema } from "../../consts/zod-schemas";
+import { clientes } from "../../models/cliente";
+import { ClienteFormatter } from "../../formatters/cliente-formatter";
+
+const router = CreateTypedRouter()
+
+router.get('/clientes', {
+  schema: {
+    summary: 'Get clientes',
+    tags: ['Clientes'],
+    response: {
+      200: z.array(clienteZodSchema)
+    },
+  },
+}, async (req, res) => {
+ 
+  const dbCliente = await clientes.find({})
+  const formattedCliente = dbCliente.map(ClienteFormatter)
+
+  return res.status(200).send(formattedCliente)
+})
+
+export const getClientes = router
