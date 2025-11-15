cacee12bbfa7775ae1ca1d1a7eed121e5005ad35
Matheus Cunha Schwab <matheus.cunha.schwab@gmail.com>
2025-11-15 19:05:26 -0300

    adição autenticação update-pacotes.ts

--- backend/src/routes/pacotes/update-pacote.ts
+++ backend/src/routes/pacotes/update-pacote.ts
@@ -3,6 +3,7 @@ import z from "zod";
 import { objectIdSchema, pacoteZodSchema } from "../../consts/zod-schemas";
 import { pacotes } from "../../models/pacote";
 import { PacoteFormatter } from "../../formatters/pacote-formatter";
+import { authValidator, roleValidator } from "../../middlewares/auth";
 
 const router = CreateTypedRouter()
 
@@ -24,7 +25,7 @@ router.put('/pacotes/:id', {
       }),
     },
   },
-}, async (req, res) => {
+}, authValidator, roleValidator('Gerente'), async (req, res) => {
   const { id } = req.params
   const { name, image, description, components, value, quantity } = req.body
 
